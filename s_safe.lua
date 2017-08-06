d9ba4b4d1fdd94e464256f0481498809b8c3fd08
majesticTim <30778942+majesticTim@users.noreply.github.com>
2017-08-06 21:03:35 +0300

    Update s_safe.lua

--- s_safe.lua
+++ s_safe.lua
@@ -1,5 +1,5 @@
 local sizeX = 120
 local sizeY = 120
-local safeZ = createColRectangle(2425.863, -1737.543, sizeX, sizeY)
+local zone = createColRectangle(2425.863, -1737.543, sizeX, sizeY)
 local radarA = createRadarArea(2425.863, -1737.543, sizeX, sizeY, 32, 178, 170, 150)
 
